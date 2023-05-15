e12f2cd04ad6f196571aad0a089393f8323c1079
정병산불곰 <myvj123@naver.com>
2023-05-15 13:51:32 +0900

    Initial commit

---
+++ src/main/java/com/mes/sajotuna/SajotunaApplication.java
@@ -0,0 +1,13 @@
+package com.mes.sajotuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SajotunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SajotunaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mes/sajotuna/SajotunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.sajotuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajotunaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
