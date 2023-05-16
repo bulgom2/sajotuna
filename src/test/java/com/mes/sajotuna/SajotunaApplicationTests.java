1ff40e904c263a51c0cd76c824cc6a76de509ee9
정병산불곰 <myvj123@naver.com>
2023-05-16 20:56:14 +0900

    test

--- src/test/java/com/mes/sajotuna/SajotunaApplicationTests.java
+++ src/test/java/com/mes/sajotuna/SajotunaApplicationTests.java
@@ -11,38 +11,38 @@ import java.util.Optional;
 @SpringBootTest
 class SajotunaApplicationTests {
 
-    @Autowired
-    private TestRepository testRepository;
-
-    @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void test(){
-        Tests tests = new Tests();
-
-        tests.setAge(51);
-
-        testRepository.save(tests);
-    }
-
-    @Test
-    void test2(){
-        Long id = 125L;
-        Optional<Tests> tests = testRepository.findById(id);
-
-        System.out.println(tests);
-        System.out.println(tests.get());
-
-        System.out.println("나이 : " + tests.get().getAge());
-
-        System.out.println();
-
-        Tests tests1 = testRepository.findByTestId(id);
-        System.out.println(tests1);
-        System.out.println("나이 : " + tests1.getAge());
-
-    }
+//    @Autowired
+//    private TestRepository testRepository;
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//    @Test
+//    void test(){
+//        Tests tests = new Tests();
+//
+//        tests.setAge(51);
+//
+//        testRepository.save(tests);
+//    }
+//
+//    @Test
+//    void test2(){
+//        Long id = 125L;
+//        Optional<Tests> tests = testRepository.findById(id);
+//
+//        System.out.println(tests);
+//        System.out.println(tests.get());
+//
+//        System.out.println("나이 : " + tests.get().getAge());
+//
+//        System.out.println();
+//
+//        Tests tests1 = testRepository.findByTestId(id);
+//        System.out.println(tests1);
+//        System.out.println("나이 : " + tests1.getAge());
+//
+//    }
 
 }
