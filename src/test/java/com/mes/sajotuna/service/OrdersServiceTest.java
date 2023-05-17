be1e291e1bcde167403659265bfcb31a5f79c3d9
정병산불곰 <myvj123@naver.com>
2023-05-17 17:56:27 +0900

    수주 뷰 연결 중

---
+++ src/test/java/com/mes/sajotuna/service/OrdersServiceTest.java
@@ -0,0 +1,27 @@
+package com.mes.sajotuna.service;
+
+import com.mes.sajotuna.entity.Orders;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class OrdersServiceTest {
+
+//    @Autowired
+//    private OrdersService ordersService;
+//
+//    @Test
+//    public void ordersTest() {
+//        List<Orders> list = ordersService.getOrders();
+//
+//        System.out.println("list : "+list);
+//
+//        for(Orders orders : list){
+//            System.out.println("주소 : " + orders);
+//            System.out.println("값 : " + orders.toString());
+//        }
+//    }
+}
