8e4432158e3f83b68978f85ed2ea16f043cd60cf
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-05-18 19:35:43 +0900

    23/05/18 엔티티 수정

--- src/main/java/com/mes/sajotuna/entity/Bom.java
+++ src/main/java/com/mes/sajotuna/entity/Bom.java
@@ -45,4 +45,6 @@ public class Bom {
     @Column(name = "bom_input_4", nullable = false)
     private Long input4;  // 재료4 투입량
 
+    @Column(name = "bom_total")
+    private Long total;  // 완제품 재고
 }
