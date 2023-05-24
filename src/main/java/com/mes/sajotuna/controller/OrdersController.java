6dee26e23ceabf6311c5c0bc4d50622c0e353c96
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-05-24 17:01:42 +0900

    23/05/24 17:01 품목정보 페이지 수정 및 연동

--- src/main/java/com/mes/sajotuna/controller/OrdersController.java
+++ src/main/java/com/mes/sajotuna/controller/OrdersController.java
@@ -141,7 +141,6 @@ public class OrdersController {
         // Orders 테이블 업데이트
         ordersRepository.save(existingOrder);
 
-        System.out.println("여기1 : " + existingOrder);
 
         return "success";
     }
